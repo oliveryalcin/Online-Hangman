8276363c3ff5f149a349f1ecd1fdf66defbde0ab
owells <oliver_wells@sfu.ca>
2020-07-23 20:26:18 +0300

    initial commit

---
+++ src/main/java/ca/cmpt213/a4/onlinehangman/model/Game.java
@@ -0,0 +1,9 @@
+package ca.cmpt213.a4.onlinehangman.model;
+
+public class Game {
+
+    private long id;
+    private Message message;
+    private int numOfGuesses;
+    private int numOfIncorrectGuesses;
+}
---
+++ src/main/java/ca/cmpt213/a4/onlinehangman/model/Words.java
@@ -0,0 +1,4 @@
+package ca.cmpt213.a4.onlinehangman.model;
+
+public class Words {
+}
